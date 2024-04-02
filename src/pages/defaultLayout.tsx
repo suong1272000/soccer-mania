6a7085e546969dadb0a99dd655f954a8e5dfa247
uoohs2 <ji0ijs2@naver.com>
2024-04-02 23:56:01 +0900

    connect

--- src/pages/defaultLayout.tsx
+++ src/pages/defaultLayout.tsx
@@ -1,18 +1,18 @@
-import SelectedListItem from "@/components/atoms/SelectedListItem";
-import Registry from "./registry";
-import HomeToolbar from "@/components/atoms/HomeToolbar";
-import ContentBox from "@/components/atoms/ContentBox";
-import { LayoutProps } from "../../.next/types/app/layout";
+import SelectedListItem from '@/components/atoms/SelectedListItem';
+import Registry from './registry';
+import HomeToolbar from '@/components/atoms/HomeToolbar';
+import ContentBox from '@/components/atoms/ContentBox';
+import { LayoutProps } from '../../.next/types/app/layout';
 
 const DefaultLayout = ({ children }: LayoutProps) => {
-	return (
-		<Registry>
-			<div style={{ display: "flex" }}>
-				<SelectedListItem />
-				<HomeToolbar />
-				<ContentBox children={children} />
-			</div>
-		</Registry>
-	);
+  return (
+    <Registry>
+      <div style={{ display: 'flex' }}>
+        <SelectedListItem />
+        <HomeToolbar />
+        <ContentBox>{children}</ContentBox>
+      </div>
+    </Registry>
+  );
 };
 export default DefaultLayout;
