eef304a65aec653c0baffa09d52f8bc8ec570891
uoohs2 <ji0ijs2@naver.com>
2024-04-02 22:20:53 +0900

    change file

---
+++ src/pages/contentLayout.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+
+const contentLayout = () => {
+	return (
+		<>
+			<Box
+				component="main"
+				sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+			></Box>
+		</>
+	);
+};
+export default contentLayout;
---
+++ src/pages/main.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function PermanentDrawerLeft() {
+  return <>메인페이지</>;
+}
