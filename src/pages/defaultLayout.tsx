a631d3fd59f5f0256e583a9effe93c8f62c24d81
uoohs2 <ji0ijs2@naver.com>
2024-04-05 00:05:20 +0900

    edit defaultLayout

--- src/pages/defaultLayout.tsx
+++ src/pages/defaultLayout.tsx
@@ -2,7 +2,11 @@ import SelectedListItem from '@/components/atoms/SelectedListItem';
 import Registry from './registry';
 import HomeToolbar from '@/components/atoms/HomeToolbar';
 import ContentBox from '@/components/atoms/ContentBox';
-import { LayoutProps } from '../../.next/types/app/layout';
+import { ReactNode } from 'react';
+
+type LayoutProps = {
+  children: ReactNode;
+};
 
 const DefaultLayout = ({ children }: LayoutProps) => {
   return (
